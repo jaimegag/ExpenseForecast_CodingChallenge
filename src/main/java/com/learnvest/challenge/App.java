d75f64f625d76f468c8fd9d494d0cea2125a368c
Jaime <jaimegag@gmail.com>
2014-09-09 16:38:45 -0400

    Working version. Adjusted future window simulation

--- src/main/java/com/learnvest/challenge/App.java
+++ src/main/java/com/learnvest/challenge/App.java
@@ -1,6 +1,7 @@
 package com.learnvest.challenge;
 
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 
 import java.util.TreeMap;
 
@@ -23,47 +24,34 @@ public class App {
     	DateTime end = new DateTime(2015, 12, 31, 0, 0, 0, 0); // December 31, 2015, 00:00:00
     	DateTime now = new DateTime(begin);
     	
-    	// We assume use case is realistic and that 28-day/monthly net gain is positive,
-    	// otherwise no major purchases would be possible.
-    	// There are monthly transactions and bi-weekly transactions, hence we can't manage monthly cycles. So I 
-    	// decided to manage by-weekly cycles (or windows) to analyze the income-expense flow, from paycheck to paycheck.
+    	// There are monthly transactions and bi-weekly transactions and so I decided to manage by-weekly cycles
+    	// (or windows) to analyze the income-expense flow, from paycheck to paycheck.
     	// Some windows will be net positive, but some other may be net negative. We assume the use case is realistic and
     	// that 28-day/monthly net gain is positive, otherwise no major purchases would be possible ever. So every window or
     	// every other window must be positive, while we may have a negative window every other window (or every three in
     	// rare occasions, or only a couple a year in really rare occasions).
-    	// Using the above assumptions these are the 3 different modes I have defined to try provide an answer to the case:
+    	// Using the above assumptions these are the 3 different modes I have defined to try provide an answer to the case,
+    	// considering that the parameters of the input file can change:
     	// Mode A (max safety):
     	//   - We assume the worst and consider we are always at the beginning of a negative window: it should be enough 
     	//     to have always enough balance in the checking to cover the maximum possible expense for the worst possible
-    	//     window (rent + groceries + credit-card) plus the checking minimum balance, of course.
-    	// Mode B (more flexible):
-    	//   - We can try to be more flexible to allow these purchases earlier in the timeline, by confirming if we have
-    	//     enough balance to cover the expenses of the window we are in (e.g: until next paycheck). But this assumption
-    	//     makes this mode not 100% safe: if we are in a positive window and we cover this window expenses very tightly
-    	//     and next window is a negative one then we may fail to maintain checking account balance above the minimum.
+    	//     day (rent + groceries + credit-card) plus the checking minimum balance, of course.
+    	// Mode B (flexible but unsafe):
+    	//   - We can try to be more flexible to allow these purchases earlier in the timeline, by simulating the future
+    	//     and checking if we have enough cash to cover the expenses of the window we are in (e.g: until next 
+    	//     paycheck) without going below the minimum. But this assumption makes this mode not 100% safe: if we are 
+    	//     in a positive window and we cover this window expenses very tightly and next window is a negative one then
+    	//     we may fail to maintain checking account balance above the minimum next week (or after).
     	// Mode C (flexible and safe):
-    	//   - We act differently depending of the type of window we are in. If we are in a negative window, we confirm we can
-    	//     cover the remaining costs of the window we are in. If we are in a positive one and we are in a use case with
-    	//     negative windows (next one) then we also simulate ahead if we will be above minimum balance during the next
-    	//     window. We only check two windows max.
-    	// I will only implement "Mode A" and "Mode C" below.
-    	Mode mode = Mode.A;
-    	
-    	// Do we have negative windows?
-    	boolean haveNegative = false;
-    	if ( ((cCase.getRent()+cCase.getGroceries())>cCase.getIncome()) ||
-    		 ((cCase.getGroceries()+cCase.getCredit().getPayment())>cCase.getIncome()) ||
-    		 ((cCase.getRent()+cCase.getGroceries()+cCase.getCredit().getPayment())>cCase.getIncome()) ) {
-    		haveNegative = true;
-    	} else {
-    		haveNegative = false; // Again, based on previous assumptions, the use case need "positive" weeks to be
-    		                      // realistic, and this means that groceries' weekly expenses have to be saller than the income.
-    	}
-    	
-    	// Timeline simulation approach to make sure we can provide the daily net worth
-    	// for the CreditCard
+    	//   - Like Mode B but simulating next window as well and confirming we will not go below the minimum balance during
+    	//     next window either. Assuming that 2 cycles of income are enough to pay for all monthly expenses, this should
+    	//     be safe enough and we don't have to simulate and check a third window. As we simulate up to 4 weeks to the
+    	//     future, this mode has the lowest performance.
+    	Mode mode = Mode.C;
+    	 	
+    	// Timeline simulation approach to make sure we can provide the daily net worth for the CreditCard
     	TreeMap<String,Float> netWorth = new TreeMap<String,Float>(); // To store the daily net worth
-    	boolean paidLastWeek = true; // To start paycheck on the second week of Jan (9th)
+    	DateTime lastPaycheckReceived = new DateTime(2014, 12, 27, 0, 0, 0, 0); // To keep a timeline pointer to when last paycheck was received
     	float currCheckBal = cCase.getChecking().getBegBal(); // Initialized to initial checking balance
     	float currCredBal = cCase.getCredit().getBegBal(); // Initialized to initial credit balance
     	// For the credit card interest expense we assume a constant monthly interest, based on the initial balance
@@ -72,11 +60,9 @@ public class App {
     	while (!now.isAfter(end)) {
     		// Process income
     		if (now.getDayOfWeek() == 6) { // Saturday (Paid on Friday and available next day)
-    			if (!paidLastWeek) {
+    			if (Days.daysBetween(lastPaycheckReceived, now).getDays() >= 14) { // More than 1 week since last paycheck. 
     				currCheckBal += cCase.getIncome();
-    				paidLastWeek = true;
-    			} else {
-    				paidLastWeek = false;
+    				lastPaycheckReceived = new DateTime(now);
     			}
     		}
     		// Process rent
@@ -99,7 +85,7 @@ public class App {
     				// First check if we have enough cash (checking balance + min balance)
     				if ((currCheckBal - p.getAmount()) >= cCase.getChecking().getMin()) {
     					// Second check if we risk going below minimum in the following days.
-    					if (isPurchaseFeasible(cCase, p, haveNegative, mode, currCheckBal, now)) { // Purchase feasible!
+    					if (isPurchaseFeasible(cCase, p, mode, currCheckBal, now, lastPaycheckReceived)) { // Purchase feasible!
     						currCheckBal -= p.getAmount();
     						p.setWhen(now.toString("yyyy-MM-dd"));
     					}
@@ -109,7 +95,6 @@ public class App {
     		// Calculate Daily Net worth
     		netWorth.put(now.toString("yyyy-MM-dd"), currCheckBal - currCredBal);
     		// Jump to next day
-    		System.out.println("->" + now.toString("yyyy-MM-dd") + ": Check[" + currCheckBal + "] Credit[" + currCredBal + "]");
     		now = now.plusDays(1);
     	}
     	// Update case with final balances and daily net worth
@@ -121,8 +106,7 @@ public class App {
         
         // Initial code: System.out.println( Json.toJson( Json.loadFromFile(args[0]) ) );
     }
-    private static boolean isPurchaseFeasible(ClientCase cc, Purchase pu, boolean haveNegative, Mode m, 
-    										  float cCBal, DateTime dt) {
+    private static boolean isPurchaseFeasible(ClientCase cc, Purchase pu, Mode m, float cCBal, DateTime dt, DateTime lastpc) {
     	// No need to do parameter error control ...
     	switch (m) {
     		case A:
@@ -130,11 +114,56 @@ public class App {
     				return true;
     			}
     			break;
+    		case B:
+    			if ( confirmCheckingStaysAboveMinimumOnFollowingWindows(cc,(cCBal - pu.getAmount()),dt,lastpc,1) )  {
+    				return true;
+    			}
+    			break;
     		case C:
+    			if ( confirmCheckingStaysAboveMinimumOnFollowingWindows(cc,(cCBal - pu.getAmount()),dt,lastpc,2) ) {
+    				return true;
+    			}
     			break;
     		default:
     			break;
     	}
     	return false; // If mode is not implemented we don't accept purchases.
+    }
+
+    private static boolean confirmCheckingStaysAboveMinimumOnFollowingWindows(ClientCase cc, float bal, DateTime currDate, DateTime lastpc, int numWindows) {
+    	// No need to do parameter error control ...
+    	float simBal = bal;
+    	DateTime day = new DateTime(currDate);
+    	day = day.plusDays(1); // We check big purchases after paying all expenses of the day, and processing income, so we can start checking next day
+    	DateTime windowEnd = new DateTime(lastpc);
+    	windowEnd = windowEnd.plusWeeks(numWindows*2); // Two weeks after
+    	while (!day.isAfter(windowEnd)) {
+    		// Calculate income
+    		if (day.getDayOfWeek() == 6) { // Saturday (Paid on Friday and available next day)
+    			if (Days.daysBetween(lastpc, day).getDays() >= 14) { // More than 1 week since last paycheck. 
+    				simBal += cc.getIncome();
+    				lastpc = new DateTime(day);
+    			}
+    		}
+    		// Calculate rent
+    		if (day.getDayOfMonth() == 1) { // 1st of the Month
+    			simBal -= cc.getRent(); // We assume the use case is realistic and rent can always be 
+    											 // paid without going below min balance if no big purchases are made
+    		}
+    		// Calculate groceries
+    		if (day.getDayOfWeek() == 6) { // Saturday
+    			simBal -= cc.getGroceries();
+    		}
+    		// Calculate credit card payment
+    		if (day.getDayOfMonth() == 20) { // 20th of the Month
+    			simBal -= cc.getCredit().getPayment();
+    		}
+    		// Check if we went below the minimum
+    		if (simBal < cc.getChecking().getMin()) {
+    			return false;
+    		}
+    		day = day.plusDays(1);
+    	}
+    	return true;
     }
 }
